5f973fb179472792feb9f232bec256670743a8d1
matthew32 <mmartinezmateo93@gmail.com>
2019-05-28 13:22:12 +0200

    remove autoload

--- src/autoload.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once("Interfaces/APIParent.php");
-require_once("Utils/XMLSerializer.php");
-require_once("Model/ResourcesAPI.php");
-require_once("RequestAPI.php");
-require_once("vendor/autoload.php");
